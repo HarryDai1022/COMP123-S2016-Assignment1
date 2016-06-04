278a625de04d6f4613f5f183c8886e97bb785b98
Harry Dai <dz655618@live.com>
2016-06-03 20:52:18 -0400

    Assignment 1

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**
+ * Author: Zun Dai
+ * Student ID: 300839401
+ * Date modified: 2016-06-03
+ * Program description: Assigment1, implementing the Hero class
+ * 
+ */
+
 namespace Assignment1
 {
     class Program
     {
+        //The main method for the driver class Program
         static void Main(string[] args)
         {
+            Hero arclight = new Hero();
+            arclight.Name = "Arc Light";
+            arclight.show();
+
+            Console.WriteLine();
+
+            arclight.fight();
+
+            Console.WriteLine();
         }
     }
 }
